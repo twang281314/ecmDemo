99355668bfdbc5e043984147b4f1df41210cc19a
jiangdeqi <36637737@qq.com>
2016-02-24 14:08:52 +0800

    update

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var staticDir = path.join(__dirname, 'public');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs-mate'));
-app.locals._layoutFile = 'layout.html';
+app.locals._layoutFile = 'layout.html';111
+
 
 //静态资源
 app.use('/public', express.static(staticDir));
