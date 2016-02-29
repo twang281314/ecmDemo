b106714cef71f0e73063fe2a08cc99990e5d7462
twang281314 <twang281314@gmail.com>
2016-02-29 15:50:29 +0800

    update

---
+++ test/app.js
@@ -0,0 +1,38 @@
+//测试Promise
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var path =require('path');
+var logger = require('../lib/logger');
+
+fs.readFileAsync(path.join(__dirname,'data.txt'),'utf-8')
+   .then((fileData)=>{
+       logger.info(fileData);
+       console.log(fileData);
+       return fs.removeAsync('demo');
+   }).then(function(data){
+      console.log(data);
+   })
+   .catch(function(error){
+       logger.error(error);
+   });
+
+/*
+var fs = require('fs');
+var path =require('path');
+
+function readFilePromise(filePath) {
+    return new Promise(function (resolve, reject) {
+        var data=fs.readFileSync(path.join(__dirname,filePath,'utf-8'),function(error,data){
+            if(error){
+                reject(new Error(error));
+            }else{
+                resolve(data);
+            }
+        });
+    });
+}
+
+readFilePromise('data.txt').then(data=>{
+  console.log(data);
+}).catch(error=>console.log(error));
+*/
