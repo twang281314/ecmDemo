1868cf44bd9022ee26f4002a52cd9c6ab399b905
jiangdeqi <36637737@qq.com>
2016-03-01 15:10:10 +0800

    grunt add

--- public/adminLte/dist/js/include.js
+++ public/adminLte/dist/js/include.js
@@ -4,12 +4,14 @@ document.write("<link rel='stylesheet' href='../public/adminLte/bootstrap/css/fo
 document.write("<link rel='stylesheet' href='../public/adminLte/bootstrap/css/iconic.css'>");
 document.write("<link rel='stylesheet' href='../public/adminLte/dist/css/AdminLTE.css'>");
 document.write("<link rel='stylesheet' href='../public/adminLte/dist/css/skins/skin-blue.min.css'>");
+document.write("<link rel='stylesheet' href='../public/adminLte/dist/css/page.css'>");
 
 //js
 document.write("<script language=javascript src='../public/adminLte/plugins/jQuery/jQuery-2.2.0.min.js'></script>");
 document.write("<script language=javascript src='../public/adminLte/bootstrap/js/bootstrap.min.js'></script>");
 document.write("<script language=javascript src='../public/adminLte/dist/js/app.min.js'></script>");
 document.write("<script language=javascript src='../public/adminLte/dist/js/iscs.js'></script>");
-
+//百度图表插件
+document.write("<script language=javascript src='../public/adminLte/dist/js/echarts.min.js'></script>");
 
 
