53249688772609f3f99f484bd6b5c8bd5c601577
jiangdeqi <36637737@qq.com>
2016-03-03 20:19:41 +0800

    update

--- controllers/base.js
+++ controllers/base.js
@@ -3,10 +3,12 @@ var fs = require('fs');
 
 var baseControl={
     
+    //员工列表页
     employeeList : function(req, res, next){
         
           res.render('base/employeeList');
     },
+    //员工列表页获取数据
     getEmployeeListData : function(req, res, next){
         
         fs.readFile('test/data.txt','utf-8',function(error,data){
