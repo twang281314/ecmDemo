20c6a6e137df7528c897065c7585a1e257ed40cf
twang281314 <twang281314@gmail.com>
2016-03-03 18:39:23 +0800

    node dubbo

---
+++ node-zookeeper-dubbo/README.md
@@ -0,0 +1,80 @@
+# node-zookeeper-dubbo
+nodejs connect dubbo by default protocol in zookeeper
+
+[![NPM version][npm-image]][npm-url]
+[![Downloads][downloads-image]][npm-url]
+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/p412726700/node-zookeeper-dubbo/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
+
+### config
+##### env
+envirmoment
+##### conn
+zookeeper conn url
+##### path
+the service you need
+##### version
+dubbo version
+
+### Example
+```javascript
+var Service=require('node-zookeeper-dubbo');
+
+var opt={
+  env:'test',
+  conn:'127.0.0.1:2180',
+  path:'com.customer.Service'
+}
+
+var method="getUserByID";
+var arg1={$class:'int',$:123}
+var args=[arg1];
+
+var service = new Service(opt);
+service.excute(method,args,function(err,data){
+  if(err){
+    console.log(err);
+    return;
+  }
+  console.log(data)
+})
+
+or
+
+service
+  .excute(method,args)
+  .then(function(data){
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+
+```
+you can use  [js-to-java](https://github.com/node-modules/js-to-java)
+```javascript
+var arg1={$class:'int',$:123};
+//equivalent
+var arg1=java('int',123);
+```
+
+### Close zookeeper connection
+
+Default the zookeeper connection is keep-alive,you can call ```service.zoo.close()``` to close the connect;
+
+
+### Contributors
+[PanEW](https://github.com/p412726700)
+
+[zhanghua](https://github.com/zhanghua499)
+
+[caomu](https://github.com/caomu)
+
+[zhchj126](https://github.com/zhchj126)
+
+[maochendong](https://github.com/maochendong)
+
+
+
+[npm-image]:http://img.shields.io/npm/v/node-zookeeper-dubbo.svg?style=flat-square
+[npm-url]:https://npmjs.org/package/node-zookeeper-dubbo?style=flat-square
+[downloads-image]:http://img.shields.io/npm/dm/node-zookeeper-dubbo.svg?style=flat-square
---
+++ node-zookeeper-dubbo/index.js
@@ -0,0 +1,230 @@
+'use strict';
+const net       = require('net');
+const hessian   = require('hessian.js');
+const url       = require('url');
+const zookeeper = require('node-zookeeper-client');
+const qs        = require('querystring');
+require('./utils');
+
+const DEFAULT_LEN = 8 * 1024 * 1024;
+
+
+/**
+ * Create a zookeeper connection
+ *
+ * @param {String} conn
+ * @param {String} env
+ * @returns {Object} zoo
+ *
+ *
+ * @constructor
+ */
+var ZK = function (conn, env) {
+
+  if (typeof ZK.instance === 'object') {
+    return ZK.instance;
+  }
+  this.conn    = conn;
+  this.env     = env;
+  this.methods = [];
+  this.connect();
+
+  ZK.instance = this;
+};
+
+ZK.prototype.connect = function (conn) {
+  !this.conn && (this.conn = conn);
+  this.client = zookeeper.createClient(this.conn);
+  this.client.connect();
+  this.client.once('connected', function connect() {
+    console.log('\x1b[32m%s\x1b[0m', 'Yeah zookeeper connected!');
+  });
+};
+
+ZK.prototype.close = function () {
+  this.client.close();
+};
+
+/**
+ * Get a zoo
+ *
+ * @param {String} path
+ * @param {Function} cb
+ */
+
+ZK.prototype.getZoo = function (path, cb) {
+  this.path = '/dubbo/' + path + '/providers';
+  var self  = this;
+
+  self.client.getChildren(self.path, handleResult);
+  function handleResult(err, children) {
+    var zoo, urlParsed;
+    if (err) {
+      return cb(err);
+    }
+    if (children && !children.length) {
+      return cb('can\'t find zoo');
+    }
+
+    for (var i = 0, l = children.length; i < l; i++) {
+      zoo = qs.parse(decodeURIComponent(children[i]));
+      if (zoo.version === self.env) {
+        break;
+      }
+    }
+    //Get the first zoo
+    urlParsed    = url.parse(Object.keys(zoo)[0]);
+    self.methods = zoo.methods.split(',');
+    cb(null, {host: urlParsed.hostname, port: urlParsed.port});
+
+
+  }
+};
+
+
+var Service = function (opt) {
+  this._version = opt.version || '2.5.3.3';
+  this._path    = opt.path;
+  this._env     = opt.env.toUpperCase();
+
+  this._attchments = {
+    $class: 'java.util.HashMap',
+    $     : {
+      path     : this._path,
+      interface: this._path,
+      version  : this._env,
+      timeout  : '60000'
+    }
+  };
+  this.zoo         = new ZK(opt.conn, this._env);
+};
+
+Service.prototype.excute = function (method, args, cb) {
+  var _method         = method;
+  var _parameterTypes = '';
+  var _arguments      = args;
+  var buffer, type, typeRef;
+
+  typeRef = {
+    boolean: 'Z', int: 'I', short: 'S',
+    long   : 'J', double: 'D', float: 'F'
+  };
+
+  if (_arguments.length) {
+    for (var i = 0, l = _arguments.length; i < l; i++) {
+      type = _arguments[i]['$class'];
+      _parameterTypes
+        += type && ~type.indexOf('.')
+        ? 'L' + type.replace(/\./gi, '/') + ';'
+        : typeRef[type];
+    }
+    buffer = this.buffer(_method, _parameterTypes, _arguments);
+  } else {
+    buffer = this.buffer(_method, '');
+  }
+  var self = this;
+
+  return new Promise(function (resolve, reject) {
+    self.zoo.getZoo(self._path, zooData);
+    function zooData(err, zoo) {
+      var client = new net.Socket();
+      var bl     = 16;
+      var host   = zoo.host;
+      var port   = zoo.port;
+      var ret    = null;
+      var chunks = [], heap;
+
+      if (err) {
+        return reject(err);
+      }
+
+      if (!~self.zoo.methods.indexOf(_method)) {
+        throw new SyntaxError("can't find this method, pls check it!");
+      }
+
+      client.connect(port, host, function () {
+        client.write(buffer);
+      });
+
+      client.on('data', function (chunk) {
+        if (!chunks.length) {
+          var arr  = [].slice.call(chunk.slice(0, 16));
+          var l, i = 0;
+          while (l = arr.pop()) {
+            bl += l * Math.pow(255, i++);
+          }
+        }
+        chunks.push(chunk);
+        heap = Buffer.concat(chunks);
+        (heap.length >= bl) && client.destroy();
+      });
+      client.on('close', function () {
+        if (heap[3] === 70) {
+          ret = heap.slice(19, heap.length - 1).toString();
+        }
+        else if (heap[15] === 3 && heap.length < 20) {//判断是否没有返回值
+          ret = 'void return';
+        }
+        else {
+          var offset = heap[16] === 145 ? 17 : 18; //判断传入参数是否有误
+          var buf    = new hessian.DecoderV2(heap.slice(offset, heap.length - 1));
+          var _ret   = buf.read();
+          if (_ret instanceof Error || offset === 18) {
+            return reject(_ret);
+          }
+          ret = JSON.stringify(_ret);
+        }
+        resolve(ret);
+      });
+    }
+  }).nodeify(cb);
+};
+
+Service.prototype.buffer = function (method, type, args) {
+  var bufferBody = this.bufferBody(method, type, args);
+  var bufferHead = this.bufferHead(bufferBody.length);
+  return Buffer.concat([bufferHead, bufferBody]);
+};
+
+Service.prototype.bufferHead = function (length) {
+  var head = [0xda, 0xbb, 0xc2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  var i    = 15;
+
+  if (length > DEFAULT_LEN) {
+    throw new Error(`Data length too large: ${length}, max payload: ${DEFAULT_LEN}`);
+  }
+  //构造body长度信息
+  if (length - 256 < 0) {
+    head.splice(i, 1, length - 256);
+  } else {
+    while (length - 256 > 0) {
+      head.splice(i--, 1, length % 256);
+      length = length / 256 | 0;
+    }
+    head.splice(i--, 1, length);
+  }
+  return new Buffer(head);
+};
+
+Service.prototype.bufferBody = function (method, type, args) {
+  var encoder = new hessian.EncoderV2();
+
+  encoder.write(this._version);
+  encoder.write(this._path);
+  encoder.write(this._env);
+  encoder.write(method);
+  encoder.write(type);
+  if (args && args.length) {
+    for (var i = 0, len = args.length; i < len; ++i) {
+      encoder.write(args[i]);
+    }
+  }
+  encoder.write(this._attchments);
+  encoder = encoder.byteBuffer._bytes.slice(0, encoder.byteBuffer._offset);
+
+  return encoder;
+};
+
+
+module.exports = Service;
+
